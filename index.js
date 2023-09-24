0dc2890879bbfa04548dccbbd3c21c975be9af7b
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-09-24 23:17:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = "Hammam Abu Shehadeh";
+// const welcomeMessage = 'Welcome to Flatbook, ';
+// const welcomeMessage = currentUser;
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = '';
+// const shortGreeting = 'Welcome, ';
+// const shortGreeting = `Welcome, ${currentUser}`;
+// const shortGreeting = `Welcome, ${currentUser[0]}!`;
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
